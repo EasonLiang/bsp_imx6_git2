d25b4260e88ddc0ca70d31955a05c60e7d3daebe
ross <ross@b067294f-1dea-0310-9683-c47a78595994>
2005-01-07 16:33:13 +0000

    Remove suite_free()
    git-svn-id: https://svn.o-hand.com/repos/matchbox/trunk/libmatchbox@1004 b067294f-1dea-0310-9683-c47a78595994

--- tests/dot-desktop.c
+++ tests/dot-desktop.c
@@ -138,7 +138,6 @@ int main(void)
   SRunner *sr = srunner_create(s);
   srunner_run_all(sr, CK_NORMAL);
   nf = srunner_ntests_failed(sr);
-  suite_free (s);
   srunner_free(sr);
   return (nf == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
