3c221a2b900e0d1507971b0b0d5cc454dc42cf3d
ross <ross@b067294f-1dea-0310-9683-c47a78595994>
2007-06-19 19:03:39 +0000

    Add TODO
    git-svn-id: https://svn.o-hand.com/repos/matchbox/trunk/matchbox-config-gtk@1572 b067294f-1dea-0310-9683-c47a78595994

--- appearance/mb-appearance.c
+++ appearance/mb-appearance.c
@@ -18,6 +18,10 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+/*
+ * TODO: if gtk+ 2.10, handle symbolic colours
+ */
+
 #include <string.h>
 #include <glib/gi18n.h>
 #include <gconf/gconf-client.h>
