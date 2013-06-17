6fb60249226eb4950bb5cc725cd0ac879e0095b6
Michael Stapelberg <stapelberg@debian.org>
2013-06-17 19:34:06 +0200

    refactor step 1: copy dh_systemd to dh_systemd_{enable,start}

--- lib/Debian/Debhelper/Sequence/systemd.pm
+++ lib/Debian/Debhelper/Sequence/systemd.pm
@@ -3,6 +3,12 @@ use warnings;
 use strict;
 use Debian::Debhelper::Dh_Lib;
 
-insert_after("dh_installinit", "dh_systemd");
+# dh_systemd_enable runs unconditionally, and before dh_installinit, so that
+# the latter can use invoke-rc.d and all symlinks are already in place.
+insert_before("dh_installinit", "dh_systemd_enable");
+
+# dh_systemd_start handles the case where there is no corresponding init
+# script, so it runs after dh_installinit.
+insert_after("dh_installinit", "dh_systemd_start");
 
 1
