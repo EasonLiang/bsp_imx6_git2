c167d9498a1386adb6bed525827e760b1a2b7104
Dan Nicholson <nicholson@endlessm.com>
2016-04-07 11:08:23 -0700

    script: Handle \ escapes in unit names properly
    Unit names can contain valid \ escapes in systemd. See systemd-escape(1)
    for examples. Text::ParseWords::shellwords breaks that since it strips \
    in addition to quotes. Instead, just use split as the only fields parsed
    will contain unit names, which can't have whitespace. Any
    leading/trailing quotes are then stripped separately.
    
    Closes: #820359

--- script/update-rc.d
+++ script/update-rc.d
@@ -446,6 +446,5 @@ sub is_initscripts_installed {
     # Check if mountkernfs is available. We cannot make inferences
     # using the running init system because we may be running in a
     # chroot
-    my @links = glob '/etc/rcS.d/S??mountkernfs.sh';
-    return @links != 0;
+    return  -f '/etc/init.d/mountkernfs.sh';
 }
