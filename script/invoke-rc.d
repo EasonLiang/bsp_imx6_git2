4ed28e496544222533ef3c3cfa3c7a678f70b990
Michael Biebl <biebl@debian.org>
2016-07-15 16:51:51 +0200

    invoke-rc.d: Allow restarting disabled-but-running services
    Restart services that are disabled but have been started manually.
    
    A start request for a running service is a nop under systemd, so we
    don't need to handle start and restart separately.

--- script/invoke-rc.d
+++ script/invoke-rc.d
@@ -380,14 +380,16 @@ RC=
 ### LOCAL POLICY: Enforce that the script/unit is enabled. For SysV init
 ### scripts, this needs a start entry in either runlevel S or current runlevel
 ### to allow start or restart.
-### Note that systemd 215 does not yet support is-enabled for SysV scripts,
-### this works only with systemd >= 220-1 (systemd-sysv-install). This can be
-### dropped after releasing stretch.
 if [ -n "$is_systemd" ]; then
     case ${ACTION} in
         start|restart)
+            # Note that systemd 215 does not yet support is-enabled for SysV scripts,
+            # this works only with systemd >= 220-1 (systemd-sysv-install). Add a
+            # simple fallback check which can be dropped after releasing stretch.
             if systemctl --quiet is-enabled "${UNIT}" 2>/dev/null || \
                ls ${RCDPREFIX}[S2345].d/S[0-9][0-9]${INITSCRIPTID} >/dev/null 2>&1; then
+                RC=104
+            elif systemctl --quiet is-active "${UNIT}" 2>/dev/null; then
                 RC=104
             else
                 RC=101
