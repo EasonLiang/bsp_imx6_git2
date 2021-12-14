mshuler/ca-certificates

commit 8033d52259172b4bddc0f8bbcb6f6566b348db72

mozilla/certdata2pem.py: print a warning for expired certificates.

mozilla/certdata2pem.py
@@ -21,11 +21,15 @@
 # USA.
 
 import base64
+import datetime
 import os.path
 import re
 import sys
 import textwrap
 import io
+
+from cryptography import x509
+
 
 objects = []
 
@@ -117,6 +121,13 @@ for obj in objects:
     if obj['CKA_CLASS'] == 'CKO_CERTIFICATE':
         if not obj['CKA_LABEL'] in trust or not trust[obj['CKA_LABEL']]:
             continue
+
+        cert = x509.load_der_x509_certificate(obj['CKA_VALUE'])
+        if cert.not_valid_after < datetime.datetime.now():
+            print('!'*74)
+            print('Trusted but expired certificate found: %s' % obj['CKA_LABEL'])
+            print('!'*74)
+
         bname = obj['CKA_LABEL'][1:-1].replace('/', '_')\
                                       .replace(' ', '_')\
                                       .replace('(', '=')\

debian/control
@@ -3,7 +3,7 @@ Section: misc
 Priority: optional
 Maintainer: Julien Cristau <jcristau@debian.org>
 Build-Depends: debhelper-compat (= 13), po-debconf
-Build-Depends-Indep: python3, openssl
+Build-Depends-Indep: python3, openssl, python3-cryptography
 Standards-Version: 4.5.0.2
 Vcs-Git: https://salsa.debian.org/debian/ca-certificates.git
 Vcs-Browser: https://salsa.debian.org/debian/ca-certificates

EasonLiang/bsp_imx6_git2

commit 4784cf2dac3b6a10f4747423ee8c7f912804feb2

mozilla/certdata2pem.py: use UTC time when checking cert validity
x509.not_valid_after returns naive UTC datetime and so does
datetime.utcnow(), so keep the time consistent when performing the
comparison.

Fixes: 8033d5225917 ("mozilla/certdata2pem.py: print a warning for expired certificates.")
Signed-off-by: Ilya Lipnitskiy <ilya.lipnitskiy@gmail.com>
Signed-off-by: Julien Cristau <jcristau@debian.org>

mozilla/certdata2pem.py
@@ -123,7 +123,7 @@ for obj in objects:
             continue
 
         cert = x509.load_der_x509_certificate(obj['CKA_VALUE'])
-        if cert.not_valid_after < datetime.datetime.now():
+        if cert.not_valid_after < datetime.datetime.utcnow():
             print('!'*74)
             print('Trusted but expired certificate found: %s' % obj['CKA_LABEL'])
             print('!'*74)